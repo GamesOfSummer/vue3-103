c2bf6e8c796f0a840420c59d77421bb07f7dce5f
Summer T <summer@summertime-development.com>
2022-02-24 15:23:22 -0600

    init, start

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+import { RouterLink, RouterView } from "vue-router";
+import HelloWorld from "@/components/HelloWorld.vue";
+import { reactive, ref, defineComponent, computed, watch } from "vue";
+
+const author = reactive({
+  getTaco: false,
+});
+
+function getTaco(event) {
+  console.log("clicked");
+  author.getTaco = !author.getTaco;
+}
+
+// watch works directly on a ref
+watch(getTaco, async (newQuestion, oldQuestion) => {
+  console.log("clicked again!");
+});
+</script>
+
+<template>
+  <div>
+    <button @click="getTaco">Click me to order a taco</button>
+    <h2>Where is my taco...</h2>
+  </div>
+</template>
