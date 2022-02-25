f9562bbc4d3fc027a9b558bbe5722d7f2ce5c74f
Summer T <summer@summertime-development.com>
2022-02-25 09:48:52 -0600

    unsure of how to ref with a watcher...

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,30 @@ import { RouterLink, RouterView } from "vue-router";
 import HelloWorld from "@/components/HelloWorld.vue";
 import { reactive, ref, defineComponent, computed, watch } from "vue";
 
-const author = reactive({
-  getTaco: false,
-});
+let getTaco = true;
+const question = ref("");
 
-function getTaco(event) {
+function getTacoClick(event) {
   console.log("clicked");
-  author.getTaco = !author.getTaco;
+  getTaco = !getTaco;
 }
 
 // watch works directly on a ref
 watch(getTaco, async (newQuestion, oldQuestion) => {
   console.log("clicked again!");
 });
+
+// watch works directly on a ref
+watch(question, async (newQuestion, oldQuestion) => {
+  console.log("clicked again!");
+});
 </script>
 
 <template>
   <div>
-    <button @click="getTaco">Click me to order a taco</button>
+    <button @click="getTacoClick">Click me to order a taco</button>
     <h2>Where is my taco...</h2>
   </div>
+
+  <input v-model="question" />
 </template>
